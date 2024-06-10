007083bb98f10f7ae581a37c31844b18d1c4187d
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-06-10 13:39:50 -0400

    Animations

--- ML Unity/Assets/Scripts/MoveToGoalAgent.cs
+++ ML Unity/Assets/Scripts/MoveToGoalAgent.cs
@@ -26,15 +26,24 @@ public class MoveToGoalAgent : Agent
     {
         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
+        float moveY = actions.DiscreteActions[1];
+        float grab = actions.DiscreteActions[0];
 
-        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * 2.5f;
+        transform.localPosition += new Vector3(moveX, moveY, moveZ) * Time.deltaTime * 2.5f;
+        // if () {
+
+        // }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
         continuousActions[0] = Input.GetAxis("Horizontal");
         continuousActions[1] = Input.GetAxis("Vertical");
+        discreteActions[0] = Input.GetKey(KeyCode.E) ? 1 : 0;
+        discreteActions[1] = Input.GetKey(KeyCode.Space) ? 1 : 0;
+
     }
 
     private void OnTriggerEnter(Collider other)
